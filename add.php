68e61346307b4291046682eee7d274c5e58dfa75
emmanuel-odonkor <46679289+emmanuel-odonkor@users.noreply.github.com>
2020-07-09 10:00:23 +0000

    Add files via upload

---
+++ add.php
@@ -0,0 +1,12 @@
+<?php
+
+//creation of namespace
+namespace boy;
+
+function add($a,$b)
+{
+	return $a+$b;
+}
+
+?>
+
---
+++ use.php
@@ -0,0 +1,8 @@
+<?php
+
+//testing 
+include("add.php");
+
+echo boy\add(2,3);
+
+?>
